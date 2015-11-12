07ba849c2fb037a19464976551571fa83866eb6d
Theresa <theresa.shan@intel.com>
2015-11-12 13:36:15 +0800

    First commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+import os
+import pprint
+import logging
+from flask import Flask
+
+app = Flask(__name__)
+
+logging.basicConfig(level=logging.DEBUG)
+
+@app.route('/')
+def hello():
+    return 'Hello World!\n' + port
+	#pprint.pformat(str(os.environ))
+
+port = os.getenv('PORT', '5000')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(port))
